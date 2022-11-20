5a8457a3d4c22120db8349f14493ca7dc40da750
Abrahamic <105780279+anogbidor@users.noreply.github.com>
2022-11-20 19:01:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,386 @@
+// let js = "great";
+// if (js === "great") alert("js great!");
+// console.log(40 + 8 + 23 - 10);
+// console.log("abraham");
+// console.log("10");
+// let firstName = "abraham";
+// console.log("abraham");
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// // variables and values
+// let john_kennedy = "hello";
+
+// let person = "abraham";
+// let firstJob = "Teacher";
+// // console.log("firstJob");
+
+// // let country = "England";
+// let continent = "Europe";
+// // let population = 16000000;
+// // console.log(country);
+// console.log(continent);
+// // console.log(population);
+
+// let age = 28;
+// let first = "abraham";
+// let fullAge = false;
+
+// let jsFun = true;
+// console.log(jsFun);
+// console.log(first);
+// // console.log (type of true)
+// console.log(typeof jsFun);
+// console.log(typeof 28);
+// //--challenge 2-----
+// let adaMi = true;
+// let country = "France";
+// let population = 24000000;
+// let language;
+// console.log(typeof adaMi);
+// console.log(country);
+// console.log(population);
+// console.log(typeof language);
+// language = "french";
+// console.log(typeof language);
+// //------end of challenge------
+
+// let age = 36;
+// age = 37;
+
+// const placeOfBirth = 1990;
+// birthYear = 1990
+
+// const birthDay = 1981;
+// const placeOfBirth = "London";
+// let placeOfResidence = "Istanbul";
+// console.log(birthDay);
+// console.log(placeOfBirth);
+// console.log(placeOfResidence);
+
+// // ----challenge 3 end here----
+// const mrAbraham = 2022 - 1990;
+// const mrsOgbidor = 2022 - 1984;
+// console.log(mrAbraham, mrsOgbidor);
+// console.log(mrAbraham * 2);
+
+// const name = "Abraham";
+// const surName = "Ogbidor";
+// console.log(name + " " + surName);
+
+// let x = 10 + 5;
+// // x = 10 + 5 = 15
+// x *= 4;
+// console.log(x);
+
+// const country = "England";
+// const continent = "Europe";
+// let population = 18;
+// const language = "English";
+
+// console.log(population / 2);
+
+// population++;
+// console.log(population > 6);
+// console.log(population < 33);
+
+// const year = 2040;
+// const myAge = 1990;
+// const muratAge = 1986;
+// console.log(year - 1986 > year - 1990);
+
+// let x, y;
+// x = y = 35 - 10 - 5;
+// console.log(x, y);
+
+// console.log(myAge, muratAge)
+
+//-------- challenge 4 end--------
+
+//--------- challenge 5 ------------
+
+// const muratKilo = 71;
+// const muratHeight = 1.87;
+// let muratBMI = muratKilo / muratHeight ** 2;
+// console.log(muratBMI);
+
+// const enginKilo = 92;
+// const enginHeight = 1.76;
+// let enginBMI = enginKilo / enginHeight ** 2;
+// console.log(enginBMI);
+
+// console.log(muratBMI > enginBMI);
+// console.log(muratBMI < enginBMI);
+// const BMI = muratBMI > enginBMI;
+// console.log(BMI);
+
+// const myName = "Murat";
+// const work = "Teacher";
+// const dateOfBirth = 1986;
+// const year = 2040;
+
+// const aboutMurat =
+//   "i'm " + myName + ", a " + (year - dateOfBirth) + " years old " + work;
+// console.log(aboutMurat);
+// // console.log(" The wolf asked little red riding hood: \" where are you going \ "he said")
+
+// const newMurat = `I'm ${myName}, a ${year - dateOfBirth} years old ${work}`;
+// console.log(newMurat);
+
+// const country = "England";
+// const continent = "Europe";
+// let population = 18;
+// const language = "English";
+
+// console.log(population / 2);
+
+// population++;
+// console.log(population > 6);
+// console.log(population < 33);
+
+// //----- challenge 6 starts here-------
+
+// const year = 2040;
+// const myAge = 1990;
+// const muratAge = 1986;
+// console.log(year - 1986 > year - 1990);
+// const explanation = `in ${country} ${continent} ${population} million people speak ${language}`;
+// console.log(explanation);
+// const my = 10;
+// const isMyAgeEligible = age <= 18;
+// if (isMyAgeEligible) {
+//   console.log("Abraham can be issued drivers license");
+// }
+// else {
+//     const yearsLeft = 18 -
+//   console.log("Abraham can't be issued  drivers license ");
+// }
+
+// const birthYear = 1986;
+// if (birthYear <= 2000) {
+//   let century = 20;
+// } else {
+//   let century = 21;
+// }
+
+// console.log(ce)
+
+// const population = 12;
+// if (population > 33) {
+//   console.log(`brazil is above average`);
+// } else {
+//   const different = 33 - population;
+//   console.log(`brazil is below ${different}`);
+// }
+
+// const muratKilo = 71;
+// const muratHeight = 1.87;
+// let muratBMI = muratKilo / muratHeight ** 2;
+// console.log(muratBMI);
+
+// const enginKilo = 92;
+// const enginHeight = 1.76;
+// let enginBMI = enginKilo / enginHeight ** 2;
+// console.log(enginBMI);
+
+// console.log(muratBMI > enginBMI);
+// console.log(muratBMI < enginBMI);
+// const BMI = muratBMI > enginBMI;
+// console.log(BMI);
+// if (enginBMI > muratBMI) {
+//   console.log(` Engin's  is ${enginBMI} higher than murat's ${muratBMI} `);
+// } else {
+//   console.log(`Murat's BMI  is ${muratBMI} higher than Engins' ${enginBMI}`);
+// }
+
+// const furkanAge = 29;
+// const furkanHeight = 1.7;
+// let furkanDetails = furkanAge / furkanHeight ** 2;
+// if (furkanDetails > 16) {
+//   console.log(`Furkan's ${furkanDetails} is Suitable for basketball`);
+// } else {
+//   console.log(`furkan  ${furkanDetails} isn't suitable for basketball`);
+// }
+// ----- calling functions-----
+// const turkey = 20;
+// const france = 15;
+// const england = 9;
+
+// function europe() {
+//   let totalPopulation = turkey + france + england;
+//   console.log(totalPopulation);
+// }
+// // console.log(totalPopulation);
+// europe();
+
+//------ functions with increement-----
+// let countsCompleted = 0;
+// countsCompleted = countsCompleted + 1;
+// console.log(countsCompleted);
+// use let to increment because const do not change
+
+// challenge for functions with increement----
+
+// let countsCompleted = 0;
+// function counted() {
+//   countsCompleted = countsCompleted + 1;
+// }
+
+// counted();
+// counted();
+// counted();
+// counted();
+// counted();
+// console.log(countsCompleted);
+
+// let coCounter = document.getElementById("coffee-counter");
+// let count = 0;
+// console.log(coCounter);
+// function increment() {
+//   count = count + 1;
+//   coCounter.innerText = count;
+// }
+//----change string to number-----
+// const girisYili = "1986";
+// console.log(Number(girisYili));
+// console.log(Number(girisYili + 18));
+// console.log(Number("Murat"));
+// console.log(typeof NaN);
+// //----type of coercion---
+// console.log("Ben " + 36 + " yasindayim ");
+// console.log("ben " + String(36) + " yasindayim");
+
+// console.log("36 " * "2");
+// let n = "1" + 1;
+// n = n - 1;
+// console.log(n);
+
+// Truthy & falsy values-----
+// falsy---
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean("bob"));
+// console.log(Boolean{});
+// console.log((""));
+
+// const money = 0;
+// if (money) {
+//   console.log("sakin harcama");
+// } else {
+//   console.log(`harcalamalarina dikkat etmelisin`);
+// }
+
+// let yukseklik;
+// if (yukseklik){
+//     console.log("yuseklik taninlamamis")
+// } else {
+//     console.log("yukseklik yok")
+// }
+
+// Equality operator-----
+// === strict equality--- flexible equality
+// const yas = 18;
+// if (yas === 18) console.log("yeni yetistkin oldum ");
+// if (yas == 18) console.log("yeni yetistkin oldum ");
+// ----------else if----------
+// const favoriteNumber = Number(prompt("My favourite number?"));
+// console.log(favoriteNumber);
+// console.log(typeof favoriteNumber);
+// // if (favoriteNumber === 23) {
+//   console.log("bir sayidir");
+// } else if (favoriteNumber === 7) {
+//   console.log("7 sansli sayim");
+// } else if (favoriteNumber === 9) {
+// } else {
+//   console.log("sayi 23 ve 7 degil");
+// }
+
+// if (favoriteNumber !== 23) {
+//   console.log("neden 23 degil");
+// }
+
+// === is string in (strict rule) if the operator is == is can allow number (true)
+// const komsuSayi = prompt("ulkenizi kac komsusu var?");
+// console.log(komsuSayi);
+// if (komsuSayi == 1) {
+//   console.log("sadece bir komsu!");
+// } else if (komsuSayi > 1) {
+//   console.log("birden fazla komsu");
+// } else {
+//   console.log("komsu yok");
+// }
+// console.log(Number(komsuSayi));
+
+//-----Boolean Logic-----
+// A. Murat has a license
+// B. murat is myopic
+// Murat has a license AND Murat is myopic both side of the ooperator must be true if not it'll be false
+// OR operator if A or B is true OR one true and one false = True
+//  more logic will go here from lesson notes------>
+
+// const ehliyetVarMi = true;
+// const gorusuVarMi = true;
+// console.log(ehliyetVarMi && gorusuVarMi);
+// console.log(ehliyetVarMi || gorusuVarMi);
+// console.log(!ehliyetVarMi);
+
+// const kullanabilirMi = ehliyetVarMi && gorusuVarMi;
+// if (ehliyetVarMi && gorusuVarMi) {
+//   console.log("murat arac kullanabilir");
+// } else {
+//   console.log("murat arac kullanamaz");
+// }
+
+// const yorgunMu = true;
+// console.log(ehliyetVarMi || gorusuVarMi || yorgunMu);
+// console.log(ehliyetVarMi && gorusuVarMi && yorgunMu);
+
+//-------conditional start------->
+// const country = "Portekiz";
+// let population = 50;
+// let language = "portuguese";
+// let haveIreland = true;
+// if (country === "English " && population < 50 && !haveIreland) {
+//   console.log("country is suitable");
+// } else if (language) {
+//   console.log("you need to relocate to Turkiye");
+// }
+const tigerPointOne = 97;
+const tigerPointTwo = 112;
+const tigerPointThree = 100;
+const snakePointOne = 109;
+const snakePointTwo = 95;
+const snakePointThree = 106;
+// for a more dynamic code we use this technique( group them using their variable)
+const tigerAverage = (tigerPointOne + tigerPointTwo + tigerPointThree) / 3;
+const snakeAverage = (snakePointOne + snakePointTwo + snakePointThree) / 3;
+console.log(tigerAverage);
+console.log(snakeAverage);
+if (tigerAverage > snakeAverage)
+  // we use this $ and curly bracket to make it readable and reusable and for syntax purposes--->
+  console.log(
+    `Tiger average (${tigerAverage}) is greater than snake average (${snakeAverage})`
+  );
+else if (snakeAverage > tigerAverage)
+  // we use this $ and curly bracket to make it readable and reusable and for syntax purposes--->
+  console.log(
+    `Snake average (${snakeAverage}) is greater than Tiger average (${tigerAverage})`
+  );
+// you don't need curly braces if the the value is one line example; if and elses---->
+else console.log(`snake and tiger is equal`);
+
+if (tigerAverage > snakeAverage && tigerAverage >= 100)
+  console.log(`Tiger average (${tigerAverage}) wins!`);
+else if (snakeAverage > tigerAverage && snakeAverage >= 100)
+  console.log(`snake average (${snakeAverage}) wins!`);
+else console.log(`snake and tiger are equal`);
+
+if (
+  tigerAverage === snakeAverage &&
+  tigerAverage >= 100 &&
+  snakeAverage >= tigerAverage
+)
+  console.log(`both Teams are equal`);
+else console.log(`both Teams are not equal`);
+
+//-----else if conditions end--------->
